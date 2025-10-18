2f082d3ae47b0c6d2941a8e8f61cbf214f7d1445
Ethem-Deli <e.deli5595@gmail.com>
2025-10-18 20:18:37 +0300

    gitignore

---
+++ src/js/firebase-config.js
@@ -0,0 +1,11 @@
+// src/js/firebase-config.js
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyB92OXlJn50jta9IHuY5czC937HMgYH2xs",
+    authDomain: "historytimeline-wdd330.firebaseapp.com",
+    projectId: "historytimeline-wdd330",
+    storageBucket: "historytimeline-wdd330.appspot.com",
+    // storageBucket: "historytimeline-wdd330.firebasestorage.app",
+    messagingSenderId: "539673015003",
+    appId: "1:539673015003:web:2621f434393950b78312fe",
+    measurementId: "G-GV6LHVLN1R"
+};
