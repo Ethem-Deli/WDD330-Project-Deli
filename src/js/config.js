d7c16c85a377173d2949270b20d1a4f434ae4d7c
Ethem-Deli <e.deli5595@gmail.com>
2025-10-18 20:15:39 +0300

    gitignore updated

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+// Example configuration file for API keys
+// Copy this file to src/js/config.js and replace with your own API key
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyDLwMRu47yXHBbfX4cimCx9BnIEtdmd0zk';
+
+
